eb39b138497e55fdb39b073854fd5cf2536e58f0
lintran-agilityio <lin.tran@asnet.com.vn>
2019-08-01 16:16:17 +0700

    Update position Workout detail highlight

--- src/components/SvgMask.js
+++ src/components/SvgMask.js
@@ -60,21 +60,43 @@ class SvgMask extends Component<Props, State> {
     const { size, screen, position, currentStepNumber } = nextProps
 
     // Check tab at ProgramDetail screen
-    const isTabProgramAtDetail = screen === 'ProgramDetailScreen' && size.y === 5
+    const isTabProgramAtDetail =
+      screen === 'ProgramDetailScreen' && size.y === 5
     const offsetAnrdoid =
-      this.isAndroid && screen === 'DiscoverScreen' && currentStepNumber === 3 ? 16 : this.isAndroid ? 27 : 0
-    const positionY = (isTabProgramAtDetail ? position.y + 7 : position.y) + offsetAnrdoid
+      this.isAndroid && screen === 'DiscoverScreen' && currentStepNumber === 3
+        ? 16
+        : this.isAndroid && screen !== 'WorkoutVideoDetailScreen'
+        ? 27
+        : 0
+    const offsetAnrdoidX =
+      this.isAndroid && screen === 'PlanProgressScreen' ? -20 : 0
+    const positionY =
+      (isTabProgramAtDetail ? position.y + 7 : position.y) + offsetAnrdoid
 
     // Check tab at DiscoverScreen
-    const isTabProgramAtHome = screen === 'DiscoverScreen' && currentStepNumber === 1
-    const isTabDiscoverAtHome = screen === 'DiscoverScreen' && currentStepNumber === 2
+    const isTabProgramAtHome =
+      screen === 'DiscoverScreen' && currentStepNumber === 1
+    const isTabDiscoverAtHome =
+      screen === 'DiscoverScreen' && currentStepNumber === 2
 
     // Hidden View with height === 4 (real style is 0)
     const isHidenHighlight = size.y === 4
-    const y = isTabProgramAtDetail || isTabProgramAtHome || isTabDiscoverAtHome ? 50 : isHidenHighlight ? 0 : size.y
-    const x = isTabProgramAtHome || isTabDiscoverAtHome ? windowDimensions.width / 2 : size.x
-    const positionX = isTabDiscoverAtHome ? screenWidth / 2 : position.x
-    if (this.props.position !== nextProps.position || this.props.size !== nextProps.size) {
+    const y =
+      isTabProgramAtDetail || isTabProgramAtHome || isTabDiscoverAtHome
+        ? 50
+        : isHidenHighlight
+        ? 0
+        : size.y
+    const x =
+      isTabProgramAtHome || isTabDiscoverAtHome
+        ? windowDimensions.width / 2
+        : size.x
+    const positionX =
+      (isTabDiscoverAtHome ? screenWidth / 2 : position.x) + offsetAnrdoidX
+    if (
+      this.props.position !== nextProps.position ||
+      this.props.size !== nextProps.size
+    ) {
       this.animate({ x, y }, { x: positionX, y: positionY })
     }
   }

3291e6d9aab66cd915e6b5e46d6cb8fe07bee004
hoaitruong-agilityio <hoai.truong@asnet.com.vn>
2019-08-20 13:39:27 +0700

    Revert position highlight on android

--- src/components/SvgMask.js
+++ src/components/SvgMask.js
@@ -1,20 +1,19 @@
 // @flow
-import React, { Component } from 'react';
-import {
-  View,
-  Animated,
-  Easing,
-  Dimensions,
-  Platform
-} from 'react-native';
+import React, { Component } from 'react'
+import { View, Animated, Easing, Dimensions, Platform } from 'react-native'
 // import { Svg } from 'expo';
-import Svg from 'react-native-svg';
-import AnimatedSvgPath from './AnimatedPath';
+import Svg from 'react-native-svg'
+import AnimatedSvgPath from './AnimatedPath'
 
-import type { valueXY } from '../types';
+import type { valueXY } from '../types'
 
-const windowDimensions = Dimensions.get('window');
-const path = (size, position, canvasSize): string => `M0,0H${canvasSize.x}V${canvasSize.y}H0V0ZM${position.x._value},${position.y._value}H${position.x._value + size.x._value}V${position.y._value + size.y._value}H${position.x._value}V${position.y._value}Z`;
+const windowDimensions = Dimensions.get('window')
+const path = (size, position, canvasSize): string =>
+  `M0,0H${canvasSize.x}V${canvasSize.y}H0V0ZM${position.x._value},${
+    position.y._value
+  }H${position.x._value + size.x._value}V${position.y._value + size.y._value}H${
+    position.x._value
+  }V${position.y._value}Z`
 const screenWidth = windowDimensions.width
 const screenHeight = windowDimensions.height
 
@@ -25,34 +24,38 @@ type Props = {
   easing: func,
   animationDuration: number,
   animated: boolean,
-  backdropColor: string,
-};
+  backdropColor: string
+}
 
 type State = {
   size: Animated.ValueXY,
   position: Animated.ValueXY,
-  canvasSize: ?valueXY,
-};
+  canvasSize: ?valueXY
+}
 
 class SvgMask extends Component<Props, State> {
   static defaultProps = {
     animationDuration: 300,
-    easing: Easing.linear,
-  };
+    easing: Easing.linear
+  }
 
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       canvasSize: {
         x: screenWidth,
-        y: screenHeight,
+        y: screenHeight
       },
       size: new Animated.ValueXY({ ...props.size, x: screenWidth / 2 }),
-      position: new Animated.ValueXY({ ...props.position, x: Platform.isPad ? -7 : 0 }),
-    };
+      position: new Animated.ValueXY({
+        ...props.position,
+        x: Platform.OS === 'ios' ? -7 : 0,
+        y: props.position.y - 1
+      })
+    }
 
-    this.state.position.addListener(this.animationListener);
+    this.state.position.addListener(this.animationListener)
     this.isAndroid = Platform.OS === 'android'
   }
 
@@ -60,30 +63,38 @@ class SvgMask extends Component<Props, State> {
     const { size, screen, position, currentStepNumber } = nextProps
 
     // Check tab at ProgramDetail screen
-    const isTabProgramAtDetail = screen === 'ProgramDetailScreen' && size.y === 5
-    const offsetAnrdoid = this.isAndroid && screen === 'DiscoverScreen' && currentStepNumber === 3
+    const isTabProgramAtDetail =
+      screen === 'ProgramDetailScreen' && size.y === 5
+    const offsetAnrdoid =
+      this.isAndroid && screen === 'DiscoverScreen' && currentStepNumber === 3
         ? 16
-        : this.isAndroid && screen !== 'WorkoutVideoDetailScreen'
-        ? 27
-        : 0
-    const offsetAnrdoidX = this.isAndroid && screen === 'PlanProgressScreen' ? -20 : 0
-    const positionY = (isTabProgramAtDetail ? position.y + 7 : position.y) + offsetAnrdoid
+        : 27
+    // : this.isAndroid && screen !== 'WorkoutVideoDetailScreen'
+    // ? 27
+    // : 0
+    // const offsetAnrdoidX = this.isAndroid && screen === 'PlanProgressScreen' ? -20 : 0
+    const offsetAnrdoidX = 0
+    const positionY =
+      (isTabProgramAtDetail ? position.y + 7 : position.y) + offsetAnrdoid
 
     // Check tab at DiscoverScreen
-    const isTabProgramAtHome = screen === 'DiscoverScreen' && currentStepNumber === 1
-    const isTabDiscoverAtHome = screen === 'DiscoverScreen' && currentStepNumber === 2
+    const isTabProgramAtHome =
+      screen === 'DiscoverScreen' && currentStepNumber === 1
+    const isTabDiscoverAtHome =
+      screen === 'DiscoverScreen' && currentStepNumber === 2
 
     // Hidden View with height === 4 (real style is 0)
     const isHidenHighlight = size.y === 4
-    const y = isTabProgramAtDetail || isTabProgramAtHome || isTabDiscoverAtHome
+    const y =
+      isTabProgramAtDetail || isTabProgramAtHome || isTabDiscoverAtHome
         ? 50
         : isHidenHighlight
         ? 0
         : size.y
-    const x = isTabProgramAtHome || isTabDiscoverAtHome
-        ? screenWidth / 2
-        : size.x
-    const positionX = (isTabDiscoverAtHome ? screenWidth / 2 : position.x) + offsetAnrdoidX
+    const x =
+      isTabProgramAtHome || isTabDiscoverAtHome ? screenWidth / 2 : size.x
+    const positionX =
+      (isTabDiscoverAtHome ? screenWidth / 2 : position.x) + offsetAnrdoidX
     if (
       this.props.position !== nextProps.position ||
       this.props.size !== nextProps.size
@@ -93,62 +104,83 @@ class SvgMask extends Component<Props, State> {
   }
 
   animationListener = (): void => {
-    const d: string = path(this.state.size, this.state.position, this.state.canvasSize);
+    const d: string = path(
+      this.state.size,
+      this.state.position,
+      this.state.canvasSize
+    )
     if (this.mask) {
-      this.mask.setNativeProps({ d });
+      this.mask.setNativeProps({ d })
     }
-  };
+  }
 
-  animate = (size: valueXY = this.props.size, position: valueXY = this.props.position): void => {
+  animate = (
+    size: valueXY = this.props.size,
+    position: valueXY = this.props.position
+  ): void => {
     if (this.props.animated) {
       Animated.parallel([
         Animated.timing(this.state.size, {
           toValue: size,
           duration: this.props.animationDuration,
-          easing: this.props.easing,
+          easing: this.props.easing
         }),
         Animated.timing(this.state.position, {
           toValue: position,
           duration: this.props.animationDuration,
-          easing: this.props.easing,
-        }),
-      ]).start();
+          easing: this.props.easing
+        })
+      ]).start()
     } else {
-      this.state.size.setValue(size);
-      this.state.position.setValue(position);
+      this.state.size.setValue(size)
+      this.state.position.setValue(position)
     }
   }
 
-  handleLayout = ({ nativeEvent: { layout: { width, height } } }) => {
+  handleLayout = ({
+    nativeEvent: {
+      layout: { width, height }
+    }
+  }) => {
     this.setState({
       canvasSize: {
         x: width,
-        y: height,
-      },
-    });
+        y: height
+      }
+    })
   }
 
   render() {
     return (
-      <View pointerEvents="box-none" style={this.props.style} onLayout={this.handleLayout}>
-        {
-          this.state.canvasSize
-            ? (
-              <Svg pointerEvents="none" width={this.state.canvasSize.x} height={this.state.canvasSize.y}>
-                <AnimatedSvgPath
-                  ref={(ref) => { this.mask = ref; }}
-                  fill={this.props.backdropColor}
-                  fillRule="evenodd"
-                  strokeWidth={1}
-                  d={path(this.state.size, this.state.position, this.state.canvasSize)}
-                />
-              </Svg>
-            )
-            : null
-        }
+      <View
+        pointerEvents="box-none"
+        style={this.props.style}
+        onLayout={this.handleLayout}
+      >
+        {this.state.canvasSize ? (
+          <Svg
+            pointerEvents="none"
+            width={this.state.canvasSize.x}
+            height={this.state.canvasSize.y}
+          >
+            <AnimatedSvgPath
+              ref={ref => {
+                this.mask = ref
+              }}
+              fill={this.props.backdropColor}
+              fillRule="evenodd"
+              strokeWidth={1}
+              d={path(
+                this.state.size,
+                this.state.position,
+                this.state.canvasSize
+              )}
+            />
+          </Svg>
+        ) : null}
       </View>
-    );
+    )
   }
 }
 
-export default SvgMask;
+export default SvgMask
